d9c2b7604e58e087fd5e3a38f0ec62ca12150d7e
Urvesh120 <urveshvaja14801@gmail.com>
2022-10-30 16:56:23 +0530

    add trusts page in landingpage

---
+++ src/app/landingpage/trusts/trusts.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TrustsComponent } from './trusts.component';
+
+describe('TrustsComponent', () => {
+  let component: TrustsComponent;
+  let fixture: ComponentFixture<TrustsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ TrustsComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TrustsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
