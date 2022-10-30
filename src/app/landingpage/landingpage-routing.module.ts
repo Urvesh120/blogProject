3a0c4097723e45bcfebf3e90a163be89444c398a
Urvesh120 <urveshvaja14801@gmail.com>
2022-10-30 16:55:54 +0530

    move home page to landing page

--- src/app/landingpage/landingpage-routing.module.ts
+++ src/app/landingpage/landingpage-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from '../home/home.component';
+import { HomeComponent } from './home/home.component';
 import { LandingpageComponent } from './landingpage.component';
 import { TrustsComponent } from './trusts/trusts.component';
 
