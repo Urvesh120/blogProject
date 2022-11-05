2ffe1f07cfccf026e3f0fb7ecbd3ac72473c6360
Urvesh120 <urveshvaja14801@gmail.com>
2022-11-05 20:31:28 +0530

    create card in trust page

--- src/app/landingpage/trusts/trusts.component.ts
+++ src/app/landingpage/trusts/trusts.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TrustsComponent implements OnInit {
 
+  currentPage = 1;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  changePage(index: number): void {
+    if(index == 1 && this.currentPage == 3){
+      this.currentPage = 0;
+    }
+    if(index == -1 && this.currentPage == 1){
+      this.currentPage = 3;
+    }
+    this.currentPage += index;
   }
 
 }
