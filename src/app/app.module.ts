7d9bc305db848bc83d84d58b6bcc70ae713435a4
Urvesh120 <urveshvaja14801@gmail.com>
2022-11-11 23:18:50 +0530

    add interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthInterceptorInterceptor } from './services/auth-interceptor.interceptor';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,11 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserAnimationsModule,
     HttpClientModule,
   ],
-  providers: [],
+  providers: [{
+    provide : HTTP_INTERCEPTORS,
+    useClass : AuthInterceptorInterceptor,
+    multi : true
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/services/auth-interceptor.interceptor.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { UtilService } from '../services/util.service';
+
+@Injectable()
+export class AuthInterceptorInterceptor implements HttpInterceptor {
+
+  constructor( private util : UtilService) {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    var token = this.util.getToken();
+    var BearerToken = "Bearer " + token;
+    const newReaquest = request.clone({
+      setHeaders : {
+        Authorization : BearerToken
+      }
+    });
+    console.log("Bearer token => ",BearerToken);
+    console.log("new request",newReaquest.headers);
+    return next.handle(newReaquest);
+  }
+}
