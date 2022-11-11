eb050394e98bb12f037061745b6f54858222e82f
Urvesh120 <urveshvaja14801@gmail.com>
2022-11-11 23:20:21 +0530

    add user-list component

---
+++ src/app/landingpage/userlist/userlist.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../../services/http.service';
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+];
+
+@Component({
+  selector: 'app-userlist',
+  templateUrl: './userlist.component.html',
+  styleUrls: ['./userlist.component.scss']
+})
+export class UserlistComponent implements OnInit {
+
+  constructor(private http : HttpService) { }
+
+  ngOnInit(): void {
+    this.http.pendungUserList().subscribe((res : any) => {
+      console.log(res);
+    });
+  }
+
+  displayedColumns: string[] = ['position', 'First Name', 'Last Name', 'Email', 'Action'];
+  dataSource = ELEMENT_DATA;
+
+  action(element : any){
+    console.log(element);
+  }
+
+}
