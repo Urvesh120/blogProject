aba4cd33d4b967e3a9cd739f9b57ac12de782d3f
Urvesh120 <urveshvaja14801@gmail.com>
2022-11-20 15:00:22 +0530

    add more fields in register form

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormArrayName, FormControl } from '@angular/forms';
 import { HttpService } from '../../services/http.service';
 import {MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition} from '@angular/material/snack-bar';
 
@@ -14,16 +14,25 @@ export class RegisterComponent implements OnInit {
   RegistrationFormGroup: any;
   horizontalPosition: MatSnackBarHorizontalPosition = 'end';
   verticalPosition: MatSnackBarVerticalPosition = 'top';
+  bloodGrouptList : any = ['A+', 'A-', 'B+', 'B_', 'O+','O-', 'AB+', 'AB-'];
 
   constructor(private fb: FormBuilder, public router: Router, private http : HttpService, private _snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
     this.RegistrationFormGroup = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
-      firstname: ['', [Validators.required]],
+      firstname: ['', Validators.required],
       lastname: ['', [Validators.required]],
+      contact: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(10)]],
+      email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required]],
       confirmpassword: [{ value: null, disabled: true }, [Validators.required]],
+      bloodgroup: ['', [Validators.required]],
+      addressLine1: ['', [Validators.required]],
+      addressLineLandmark: [''],
+      addressLineCity: ['', [Validators.required]],
+      addressLinePincode: ['', [Validators.required]],
+      occupationBusiness: ['', [Validators.required]],
+      description: [''],
     });
   }
 
@@ -52,6 +61,10 @@ export class RegisterComponent implements OnInit {
     return this.RegistrationFormGroup.get('lastname');
   }
 
+  get contact() {
+    return this.RegistrationFormGroup.get('contact');
+  }
+
   get email() {
     return this.RegistrationFormGroup.get('email');
   }
@@ -64,29 +77,60 @@ export class RegisterComponent implements OnInit {
     return this.RegistrationFormGroup.get('confirmpassword');
   }
 
+  get bloodgroup() {
+    return this.RegistrationFormGroup.get('bloodgroup');
+  }
+
+  get addressLine1() {
+    return this.RegistrationFormGroup.get('addressLine1');
+  }
+
+  get addressLineCity() {
+    return this.RegistrationFormGroup.get('addressLineCity');
+  }
+
+  get addressLinePincode() {
+    return this.RegistrationFormGroup.get('addressLinePincode');
+  }
+
+  get occupationBusiness() {
+    return this.RegistrationFormGroup.get('occupationBusiness');
+  }
+
+
   redirectToLogin(){
     this.router.navigate(['auth/login']);
   }
 
   register(){
+    let address = 
+      this.RegistrationFormGroup.value.addressLine1 + "," +
+      this.RegistrationFormGroup.value.addressLineLandmark + "," +
+      this.RegistrationFormGroup.value.addressLineCity + "-" +
+      this.RegistrationFormGroup.value.addressLinePincode + ".";
     let data = {
-      "email": this.RegistrationFormGroup.value.email,
       "firstName": this.RegistrationFormGroup.value.firstname,
       "lastName": this.RegistrationFormGroup.value.lastname,
+      "email": this.RegistrationFormGroup.value.email,
       "password": this.RegistrationFormGroup.value.password,
+      "address": address,
+      "contact": this.RegistrationFormGroup.value.contact,
+      "bloodgroup": this.RegistrationFormGroup.value.bloodgroup,
+      "occupationBusiness": this.RegistrationFormGroup.value.occupationBusiness,
+      "description": this.RegistrationFormGroup.value.description,
     }
     console.groupCollapsed(data);
-    this.http.register(data).subscribe((res : any) =>{
-      if(res.message = "User registration requested successfully."){
-        localStorage.removeItem('userEmailId');
-        this._snackBar.open("Register Request sent Successfully", "close",{
-          duration : 5 * 1000,
-          horizontalPosition: this.horizontalPosition,
-          verticalPosition: this.verticalPosition,
-        });
-        this.router.navigate(['/home']);
-      }
-    });
+    // this.http.register(data).subscribe((res : any) =>{
+    //   if(res.message = "User registration requested successfully."){
+    //     localStorage.removeItem('userEmailId');
+    //     this._snackBar.open("Register Request sent Successfully", "close",{
+    //       duration : 5 * 1000,
+    //       horizontalPosition: this.horizontalPosition,
+    //       verticalPosition: this.verticalPosition,
+    //     });
+    //     this.router.navigate(['/home']);
+    //   }
+    // });
   }
 
 }
