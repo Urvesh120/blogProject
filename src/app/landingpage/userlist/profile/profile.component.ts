7dc705bc20722f49ff4394ef5b5e9150672c99ed
Urvesh120 <urveshvaja14801@gmail.com>
2022-12-03 18:45:07 +0530

    add details in profile dialog

--- src/app/landingpage/userlist/profile/profile.component.ts
+++ src/app/landingpage/userlist/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { HttpService } from '../../../services/http.service';
 
 @Component({
@@ -13,12 +14,22 @@ export class ProfileComponent implements OnInit {
   isAdmin : any;
   isRegistered : any;
   id : any;
+  blankImage = 'assets/images/blank-profile.jpg';
+  profilePic : any;
 
   constructor(private dialogRef: MatDialogRef<ProfileComponent>,
-    @Inject(MAT_DIALOG_DATA) public userData: any, private http : HttpService) { }
+    @Inject(MAT_DIALOG_DATA) public userData: any, private http : HttpService,
+    private sanitizer: DomSanitizer) { }
 
   ngOnInit(): void {
     this.profileData = this.userData.data.profileData;
+    this.id = this.profileData.id
+    if(!!this.profileData.picture){
+      this.profilePic = this.sanitizer.bypassSecurityTrustResourceUrl(this.profileData.picture);
+    }
+    else{
+      this.profilePic = this.blankImage;
+    }
     this.isAdmin = this.userData.data.isAddmin;
     this.isRegistered = this.userData.data.isRegistered;
   }
@@ -28,7 +39,7 @@ export class ProfileComponent implements OnInit {
       "id": this.id,
       "action": status,
     }
-    // this.http.requestAction(data).subscribe((res : any) =>{});
+    this.http.requestAction(data).subscribe((res : any) =>{});
   }
 
 }
