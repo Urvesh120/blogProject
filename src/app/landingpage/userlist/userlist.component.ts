80b523a6b3c99a08fc69dc868fcdd8fa455ca40e
Urvesh120 <urveshvaja14801@gmail.com>
2022-12-03 18:45:39 +0530

    change css in user list

--- src/app/landingpage/userlist/userlist.component.ts
+++ src/app/landingpage/userlist/userlist.component.ts
@@ -1,21 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpService } from '../../services/http.service';
-import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { MatDialog } from '@angular/material/dialog';
 import { ProfileComponent } from './profile/profile.component';
 import { MatTableDataSource } from '@angular/material/table';
+import { MatTabChangeEvent } from '@angular/material/tabs';
 
-export interface pendingUser {
-  id: string;
-  firstName: string;
-  lastName: string;
-  email: string;
-}
-
-export interface registeredUser {
-  id: string;
-  firstName: string;
-  lastName: string;
-  email: string;
+export interface userData {
+  id: any;
+  firstName: any;
+  lastName: any;
+  email: any;
+  contact: any;
+  bloodGroup: any;
+  occupation: any;
+  description : any;
+  address: any;
+  picture: any;
 }
 
 @Component({
@@ -25,14 +25,24 @@ export interface registeredUser {
 })
 export class UserlistComponent implements OnInit {
 
-  pendingUserList : pendingUser[] = [];
-  registeredUserList : registeredUser[] = [];
-  registeredUserDataSource !: MatTableDataSource<registeredUser>;
+  pendingUserList : userData[] = [];
+  registeredUserList : userData[] = [];
+  registeredUserDataSource : any;
+  pendingUserDataSource : any;
+  dataSource : any;
   emailId : any;
   isLogedIn = false;
   isAdmin = false;
   isUser = false;
-  columns = ['First Name', 'Last Name', 'Email', 'Action'];
+  columns = [
+    'First_Name', 
+    'Last_Name', 
+    'Email', 
+    'Contact', 
+    'Bloodgroup', 
+    'Occupation', 
+    'Address',  
+    'Action'];
 
   constructor(private http : HttpService, private dialog: MatDialog) { }
 
@@ -43,20 +53,42 @@ export class UserlistComponent implements OnInit {
       if(this.emailId == "admin@email.com"){
         this.isAdmin = true;
         this.http.pendingUserList().subscribe((res : any) => {
-          this.pendingUserList = res.PendingRequests;
+          this.pendingUserList = res;
+          this.pendingUserDataSource = new MatTableDataSource<userData>(this.pendingUserList);
+          this.pendingUserDataSource.filterPredicate = function(data : userData, filter : any): boolean {
+            return data.firstName.toLowerCase().includes(filter) || 
+            data.lastName.toLowerCase().includes(filter) || 
+            data.email.toLowerCase().includes(filter) || 
+            data.contact.toLowerCase().includes(filter) || 
+            data.bloodGroup.toLowerCase().includes(filter) || 
+            data.occupation.toLowerCase().includes(filter) || 
+            data.address.toString().includes(filter)};
         });
       }
       this.isUser = true;
       this.http.userlist().subscribe((res : any) => {
-        this.registeredUserList = res.list;
-        this.registeredUserDataSource = new MatTableDataSource<registeredUser>(this.registeredUserList);
+        this.registeredUserList = res;
+        this.registeredUserDataSource = new MatTableDataSource<userData>(this.registeredUserList);
+        this.registeredUserDataSource.filterPredicate = function(data : userData, filter : any): boolean {
+          return data.firstName.toLowerCase().includes(filter) || 
+          data.lastName.toLowerCase().includes(filter) || 
+          data.email.toLowerCase().includes(filter) || 
+          data.contact.toLowerCase().includes(filter) || 
+          data.bloodGroup.toLowerCase().includes(filter) || 
+          data.occupation.toLowerCase().includes(filter) || 
+          data.address.toString().includes(filter)};
       });
     }
   }
 
-  applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.registeredUserDataSource.filter = filterValue.trim().toLowerCase();
+  registerListFilter(event: Event) {
+    const filter = (event.target as HTMLInputElement).value;
+    this.registeredUserDataSource.filter = filter.trim().toLowerCase();
+  }
+
+  pendingListFilter(event: Event) {
+    const filter = (event.target as HTMLInputElement).value;
+    this.pendingUserDataSource.filter = filter.trim().toLowerCase();
   }
 
   openDialog(profileData : any, isRegistered : any){  
@@ -66,9 +98,12 @@ export class UserlistComponent implements OnInit {
       "isRegistered" : isRegistered
     }
     const dialogRef = this.dialog.open(ProfileComponent, {
-      // height: '300px',
       width: '500px',
       data: {data},
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      this.ngOnInit();
     });
   }
 }
