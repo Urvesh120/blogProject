f343d6f09ac27376a0fa7cf7c6bb8297b77b109f
sagar bakraniya <sagarbakraniya5@gmail.com>
2023-01-27 17:33:33 +0530

    Nondera - landing page - Chagne design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthInterceptorInterceptor } from './services/auth-interceptor.interceptor';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { CoustomtoastrComponent } from './coustomtoastr/coustomtoastr.component';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -19,6 +20,7 @@ import { CoustomtoastrComponent } from './coustomtoastr/coustomtoastr.component'
     BrowserAnimationsModule,
     HttpClientModule,
     MatProgressSpinnerModule,
+    MatIconModule
   ],
   providers: [{
     provide : HTTP_INTERCEPTORS,
