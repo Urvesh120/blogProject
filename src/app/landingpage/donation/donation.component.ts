e39d319097e0d419ce2add79e1974ec90c61dd02
Urvesh120 <114854913+Urvesh120@users.noreply.github.com>
2023-06-17 17:22:12 +0530

    donation page design

--- src/app/landingpage/donation/donation.component.ts
+++ src/app/landingpage/donation/donation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-donation',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DonationComponent implements OnInit {
 
-  constructor() { }
+  colorControl!: FormControl;
+  donationFormGroup: any;
+  isNandikeshwarMahadevTrustRadioButtonSelected = true;
+
+  constructor(private fb: FormBuilder) {
+    
+   }
 
   ngOnInit(): void {
+    this.donationFormGroup = this.fb.group({
+      trustRadioButton: ['nandikeshwarMahadevTrust', [Validators.required]],
+      password: ['', [Validators.required]],
+    })
+  }
+
+  changeRadioButton(event : any){
+    if(event.target.value == 'nandikeshwarMahadevTrust'){
+      this.isNandikeshwarMahadevTrustRadioButtonSelected = true;
+    }
+    if(event.target.value == 'nandikeshwarCheritableTrust'){
+      this.isNandikeshwarMahadevTrustRadioButtonSelected = false;
+    }
   }
 
 }
